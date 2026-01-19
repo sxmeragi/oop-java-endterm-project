180f8bc9f4b04f3e8c350f9d21d9b3507ce5ae4b
eslishnah <sandzharfazyl@gmail.com>
2026-01-19 14:44:05 +0500

    created genre entity

--- src/main/java/com/aitu/oop/App.java
+++ src/main/java/com/aitu/oop/App.java
@@ -1,11 +1,15 @@
 package com.aitu.oop;
 
+import com.aitu.oop.entity.Genre;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
+
+
         System.out.println("Hello World!");
     }
 }
---
+++ src/main/java/com/aitu/oop/entity/Genre.java
@@ -0,0 +1,36 @@
+package com.aitu.oop.entity;
+
+
+public class Genre {
+
+    private int id;
+    private String name;
+
+    public Genre() {
+    }
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public Genre(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
