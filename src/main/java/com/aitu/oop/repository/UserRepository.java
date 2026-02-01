745de3883aa7b327d1fd0d9dd61b8e24ec87e1a3
unknown <amirkaragerei@gmail.com>
2026-02-01 23:11:47 +0600

    Add user repository

---
+++ src/main/java/com/aitu/oop/repository/UserRepository.java
@@ -0,0 +1,61 @@
+package com.aitu.oop.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aitu.oop.DatabaseConfig.DatabaseConfig;
+import com.aitu.oop.entity.User;
+
+public class UserRepository {
+
+    public void addUser(User user) {
+        String sql = """
+                    INSERT INTO users(username, email, password, role)
+                    VALUES(?,?,?,?)
+                """;
+        try (Connection connection = DatabaseConfig.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, user.getUserName());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getPassword());
+            ps.setString(4, user.getRole());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        String sql = """
+                    SELECT * FROM users
+                """;
+        try (Connection connection = DatabaseConfig.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql);
+
+                ResultSet rs = ps.getResultSet()) {
+
+            while (rs.next()) {
+                users.add(new User(
+                        rs.getInt("id"),
+                        rs.getString("username"),
+                        rs.getString("email"),
+                        rs.getString("password"),
+                        rs.getString("role")
+
+                ));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return users;
+
+    }
+
+}
