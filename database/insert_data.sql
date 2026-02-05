66e6bb50a30495c49010fec7d3476602fd5d7423
Sxmeragi <sxmeragi@gmail.com>
2026-02-06 04:20:24 +0500

    Add more data to movie

--- database/insert_data.sql
+++ database/insert_data.sql
@@ -12,4 +12,9 @@ INSERT INTO movies (title, release_year, rating, genre_id) VALUES
 ('The Dark Knight', 2008, 9.0, 2),
 ('STAR WARS: A New Hope', 1977, 9.1, 3),
 ('The Hangover', 2009, 7.1, 4),
-('It', 2017, 7.9, 5)
+('It', 2017, 7.9, 5),
+('Interstellar', 2014, 8.6, 3),
+('Pulp Fiction', 1994, 8.9, 2),
+('Forrest Gump', 1994, 8.8, 1),
+('The Matrix', 1999, 8.7, 3),
+('Joker', 2019, 8.5, 2);
